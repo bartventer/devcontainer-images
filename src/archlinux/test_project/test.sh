aa54cdb4ad98e2651cba00d6640b0cb33604448d
Bart <bartventer@outlook.com>
2024-04-17 00:56:20 +0000

    feat(src/archlinux): Update archlinux features

--- src/archlinux/test_project/test.sh
+++ src/archlinux/test_project/test.sh
@@ -7,12 +7,19 @@ source test-utils.sh
 # Run common tests
 checkCommon
 
-check "jq" "jq --version"
 check "curl" "curl --version"
 check "git" "git --version"
 check "zsh" "zsh --version"
 check "Oh My Zsh! theme" "test -e \"$HOME/.oh-my-zsh/custom/themes/devcontainers.zsh-theme\""
 check "zsh theme symlink" "test -e \"$HOME/.oh-my-zsh/custom/themes/codespaces.zsh-theme\""
+
+check "go" "go version"
+check "node" "node --version"
+check "npm" "npm --version"
+check "yarn" "yarn --version"
+check "make" "make --version"
+check "jq" "jq --version"
+check "yq" "yq --version"
 
 # Report results
 reportResults
