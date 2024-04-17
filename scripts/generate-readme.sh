b26591a2a060ade8f1184cf58f695ca48e7cf016
Bart <bartventer@outlook.com>
2024-04-17 23:39:01 +0000

    ci(workflows): update sed delimiter in  generate-readme script [skip ci]

--- scripts/generate-readme.sh
+++ scripts/generate-readme.sh
@@ -35,9 +35,9 @@ sed -e "s/{{name}}/$(jq -r '.name' "$METADATA_FILE")/g" \
     -e "s/{{definitionType}}/$(jq -r '.definitionType' "$METADATA_FILE")/g" \
     -e "s/{{containerHostOSSupport}}/$(jq -r '.containerHostOSSupport | join(", ")' "$METADATA_FILE")/g" \
     -e "s/{{containerOS}}/${CONTAINER_OS}/g" \
-    -e "s/{{publishedImageArchitecture}}/$(jq -r '.platforms | join(", ")' "$METADATA_FILE")/g" \
+    -e "s|{{publishedImageArchitecture}}|$(jq -r '.platforms | join(", ")' "$METADATA_FILE")|g" \
     -e "s/{{languages}}/$(jq -r '.languages | join(", ")' "$METADATA_FILE")/g" \
     -e "s|{{imageNames}}|${IMAGE_NAMES}|g" \
-    doc/README-template.md | tr '@' '\n' >"src/${IMAGE_NAME}/README.md"
+    doc/README-template.md | tr '@' '\n' >"src/${IMAGE_NAME}/README2.md"
 
 echo "✔️ OK. README.md file is generated for $IMAGE_NAME."
