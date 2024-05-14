937820655f984258897ea315b97fdf0dcabae34a
Bart <bartventer@outlook.com>
2024-05-14 18:22:41 +0200

    test(base-archlinux): Remove check for `go` due to omission from `common-utils` feature

--- src/base-archlinux/test_project/test.sh
+++ src/base-archlinux/test_project/test.sh
@@ -13,7 +13,6 @@ check "zsh" "zsh --version"
 check "Oh My Zsh! theme" "test -e \"$HOME/.oh-my-zsh/custom/themes/devcontainers.zsh-theme\""
 check "zsh theme symlink" "test -e \"$HOME/.oh-my-zsh/custom/themes/codespaces.zsh-theme\""
 
-check "go" "go version"
 check "node" "node --version"
 check "npm" "npm --version"
 check "yarn" "yarn --version"
