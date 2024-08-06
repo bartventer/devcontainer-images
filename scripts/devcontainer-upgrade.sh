7ec97192d9b539fdb451da1eddcd1b4b6ab524a0
Bart Venter <bartventer@outlook.com>
2024-08-06 19:14:47 +0000

    chore(scripts): Update devcontainer-upgrade script [skip ci]

--- scripts/devcontainer-upgrade.sh
+++ scripts/devcontainer-upgrade.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 echo "Upgrading the devcontainer lockfiles..."
 
-folders_containing_devcontainer=$(find . -name .devcontainer -type d -exec dirname {} \;)
-for folder in $folders_containing_devcontainer; do
-    echo "Upgrading lockfiles in $folder..."
-    devcontainer upgrade --workspace-folder "$folder" --config ".devcontainer/devcontainer.json"
+devcontainer_dirs=$(find . -name .devcontainer -type d -exec dirname {} \;)
+for dir in $devcontainer_dirs; do
+    echo "Upgrading lockfiles in $dir..."
+    devcontainer upgrade --workspace-folder "$dir" --config ".devcontainer/devcontainer.json"
 done
 
 echo "✅ Done upgrading the devcontainer lockfiles."
