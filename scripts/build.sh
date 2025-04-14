125139299f42b3fc391f54a1c47975d6c6d7fdb6
Bart Venter <bartventer@outlook.com>
2025-04-14 13:37:35 +0000

    chore(build): Update build script to require version argument and improve usage message

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,50 +1,54 @@
 #!/usr/bin/env bash
 # This script builds, tags, and pushes the image to the container registry.
-# The script expects the image name and the path to the configuration directory as arguments.
-# Usage: ./scripts/build.sh <image-name> <config-path>
-# Example: ./scripts/build.sh devcontainer-images/archlinux src/archlinux
 
 set -euo pipefail
 
-IMAGE_NAME=$1
-DRYRUN="${2:-false}"
+usage() {
+	local message=$1 #Optional error message
+	if [[ -n "$message" ]]; then
+		echo "Error: $message"
+		echo
+	fi
+	echo "Usage: $0 <image-name> <version>"
+	echo "Example: $0 archlinux 20231010.123456"
+	echo "Note: use DRYRUN=true to skip the build and push."
+}
 
+IMAGE_NAME=${1:?"$(usage "Image name is required.")"}
+NEXT_VERSION=${2:?"$(usage "Version is required.")"}
+
+DRYRUN=${DRYRUN:-false}
 CR="${CR:-ghcr.io}"
 GITHUB_REPOSITORY="${GITHUB_REPOSITORY:-$(git config --get remote.origin.url | sed 's/.*://;s/.git$//')}"
 
-if [[ -z "${IMAGE_NAME}" ]]; then
-    echo "(!) Image name not provided"
-    exit 1
-elif [[ ! -f "src/${IMAGE_NAME}/metadata.json" ]]; then
-    echo "(!) Metadata file not found or empty: src/${IMAGE_NAME}/metadata.json"
-    exit 1
+if [[ ! -f "src/${IMAGE_NAME}/metadata.json" ]]; then
+	echo "(!) Metadata file not found or empty: src/${IMAGE_NAME}/metadata.json"
+	exit 1
 fi
+
+echo "==============================================="
+echo "🚀 Running image build, tag and push"
+echo "(*) Image name: ${IMAGE_NAME}"
+echo "(*) Version: ${NEXT_VERSION}"
+echo "(*) Container registry: ${CR}"
+echo "(*) GitHub repository: ${GITHUB_REPOSITORY}"
+echo "(*) Dry run: ${DRYRUN}"
+echo "==============================================="
 
 METADATA=$(jq -r '.' "src/${IMAGE_NAME}/metadata.json")
-
-DATE_TAG=$(date +%Y%m%d)
-BUILD_JOB_NUMBER="${GITHUB_RUN_ID:-$(date +%s)}"
-NEW_VERSION="${DATE_TAG}.${BUILD_JOB_NUMBER}"
-
-echo "(*) Running image build, tag and push for ${IMAGE_NAME} with version ${NEW_VERSION}"
 BUILD_OUTPUT="src/${IMAGE_NAME}/build-output.json"
-if [ "${DRYRUN}" = "false" ]; then
-    devcontainer build \
-        --log-level debug \
-        --workspace-folder "src/${IMAGE_NAME}" \
-        --image-name "${CR}/${GITHUB_REPOSITORY}/${IMAGE_NAME}:latest" \
-        --image-name "${CR}/${GITHUB_REPOSITORY}/${IMAGE_NAME}:${NEW_VERSION}" \
-        --platform "$(echo "${METADATA}" | jq -r '.platforms | join(",")')" \
-        --push >"${BUILD_OUTPUT}"
-
-    ./scripts/generate-readme.sh "${IMAGE_NAME}"
-
-    ./scripts/create-pr.sh "${IMAGE_NAME}" "${NEW_VERSION}"
-
+if [[ "${DRYRUN}" == "false" ]]; then
+	devcontainer build \
+		--log-level debug \
+		--workspace-folder "src/${IMAGE_NAME}" \
+		--image-name "${CR}/${GITHUB_REPOSITORY}/${IMAGE_NAME}:latest" \
+		--image-name "${CR}/${GITHUB_REPOSITORY}/${IMAGE_NAME}:${NEXT_VERSION}" \
+		--platform "$(echo "${METADATA}" | jq -r '.platforms | join(",")')" \
+		--push >"${BUILD_OUTPUT}"
 else
-    echo "(*) Dry run enabled. Skipping the build and push."
+	echo "(*) Dry run enabled. Skipping the build and push."
 fi
 
-echo "✔️ OK. Image built, tagged and pushed to the registry."
 echo "Build output (${BUILD_OUTPUT}):"
 jq . "${BUILD_OUTPUT}"
+echo
