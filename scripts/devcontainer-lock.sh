60c10f1f0bb27130c3c6a5531d422ea166539cfe
Bart Venter <bartventer@outlook.com>
2025-04-14 13:34:36 +0000

    chore: Rename metadata schema file

--- scripts/devcontainer-lock.sh
+++ scripts/devcontainer-lock.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo "Upgrading the devcontainer lockfiles..."
 
-devcontainer_dirs=$(find . -name .devcontainer -type d -exec dirname {} \;)
+devcontainer_dirs=$(find . -name .devcontainer -type d -print)
 for dir in $devcontainer_dirs; do
     echo "Upgrading lockfiles in $dir..."
     devcontainer upgrade --workspace-folder "$dir" --config ".devcontainer/devcontainer.json"
