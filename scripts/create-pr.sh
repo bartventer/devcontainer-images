d53f7f0a12a3ff3f8343b4e7cd576991b661523c
Bart Venter <bartventer@outlook.com>
2025-04-14 13:37:54 +0000

    chore(scripts): Enhance create-pr.sh with improved usage message and error handling

--- scripts/create-pr.sh
+++ scripts/create-pr.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 # Creates a PR for automated documentation update.
-# Usage: ./scripts/create-pr.sh <image-name> <version>
-# Example: ./scripts/create-pr.sh archlinux 20231010.123456
 
 set -euo pipefail
 
-IMAGE_NAME=${1}
-VERSION=${2}
+usage() {
+	local message=$1 #Optional error message
+	if [[ -n "$message" ]]; then
+		echo "Error: $message"
+		echo
+	fi
+	echo "Usage: $0 <image-name> <version>"
+	echo "Example: $0 archlinux 20231010.123456"
+}
 
-echo "Creating a PR for the automated documentation update..."
-echo "Image name: ${IMAGE_NAME}"
-echo "Version: ${VERSION}"
+IMAGE_NAME=${1:?"$(usage "Image name is required.")"}
+VERSION=${2:?"$(usage "Version is required.")"}
+
+echo "==============================================="
+echo "📄 Creating a PR for the automated documentation update..."
+echo "(*) Image name: ${IMAGE_NAME}"
+echo "(*) Version: ${VERSION}"
+echo "==============================================="
+
+if [[ "${DRYRUN:-false}" == "true" ]]; then
+	echo "DRYRUN is enabled. Skipping the PR creation."
+	exit 0
+fi
 
 # Switch back to the current branch on exit
 current_branch=$(git rev-parse --abbrev-ref HEAD)
@@ -28,25 +43,26 @@ git checkout -b "$branch"
 git add src/"${IMAGE_NAME}"/README.md
 
 git commit -S \
-    -m "chore(docs/${IMAGE_NAME}): Automated documentation update [skip ci]" \
-    -m "This PR updates the README file for the ${IMAGE_NAME} image to version ${VERSION}." \
-    -m "Co-authored-by: Bart Venter <bartventer@outlook.com>"
+	-m "chore(docs/${IMAGE_NAME}): Automated documentation update to version ${VERSION} [skip ci]" \
+	-m "This PR updates the README file for the ${IMAGE_NAME} image to version ${VERSION}." \
+	-m "Co-authored-by: Bart Venter <bartventer@outlook.com>"
 
 git push origin "$branch"
 
 pr_url=$(
-    gh pr create \
-        --fill \
-        --label "documentation" \
-        --assignee "${GITHUB_ACTOR}" \
-        --reviewer "${GITHUB_ACTOR}"
+	gh pr create \
+		--fill \
+		--label "documentation" \
+		--assignee "${GITHUB_ACTOR}" \
+		--reviewer "${GITHUB_ACTOR}"
 )
 
 pr_number=$(echo "$pr_url" | grep -o '[0-9]\+$')
 
 gh pr merge "$pr_number" \
-    --auto \
-    --rebase \
-    --delete-branch
+	--auto \
+	--rebase \
+	--delete-branch
 
-echo "Done. Created a PR (${pr_url}) for the automated documentation update and enabled auto-merge."
+echo "✅ Done. Created a PR (${pr_url}) for the automated documentation update and enabled auto-merge."
+echo
