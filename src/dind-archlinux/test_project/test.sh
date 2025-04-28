637bf1a977ea3a7c4ea5db166bf276b010d0ac12
Bart Venter <bartventer@outlook.com>
2025-04-28 09:46:45 +0000

    feat: Add Docker-in-Docker image for Arch Linux

---
+++ src/dind-archlinux/test_project/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+# shellcheck disable=SC1091
+source test-utils.sh
+
+# Run common tests
+checkCommon
+
+check "docker-daemon-check" bash -c "./_docker_daemon_check.sh"
+check "version" docker --version
+check "docker-init-exists" bash -c "ls /usr/local/share/docker-init.sh"
+
+check "log-exists" bash -c "ls /tmp/dockerd.log"
+check "log-for-completion" bash -c "cat /tmp/dockerd.log | grep 'Daemon has completed initialization'"
+check "log-contents" bash -c "cat /tmp/dockerd.log | grep 'API listen on /var/run/docker.sock'"
+
+# Report results
+reportResults
